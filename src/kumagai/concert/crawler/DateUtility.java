3dab444765e694c03c098a24e0c2bc5bea38d6ee
kumagai <kumagai@toshiba-tops.co.jp>
2017-09-01 12:46:37 +0900

    new

---
+++ src/kumagai/concert/crawler/DateUtility.java
@@ -0,0 +1,18 @@
+package kumagai.concert.crawler;
+
+/**
+ * 文字列ユーティリティ
+ * @author kumagai
+ */
+public class DateUtility
+{
+	/**
+	 * 日付文字列の整形
+	 * @param date 対象の文字列
+	 * @return 整形した日付文字列
+	 */
+	static public String trimDate(String date)
+	{
+		return date;
+	}
+}
