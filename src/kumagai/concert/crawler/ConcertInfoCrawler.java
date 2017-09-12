af9b4fc0c3b1851b9dabedcc022da3451bfabb22
kumagai <kumagai@toshiba-tops.co.jp>
2017-09-12 12:48:37 +0900

    文字コード対応

--- src/kumagai/concert/crawler/ConcertInfoCrawler.java
+++ src/kumagai/concert/crawler/ConcertInfoCrawler.java
@@ -8,9 +8,16 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
 
+/**
+ * コンサート情報収集処理
+ * @author kumagai
+ */
 public class ConcertInfoCrawler
 {
-	static public void main(String[] args) throws IOException
+	static private final String [] encodes = { "UTF-8", "Shift_JIS", "EUC-JP" };
+
+	static public void main(String[] args)
+		throws IOException
 	{
 		System.setProperty("proxySet", "true");
 		System.setProperty("proxyHost", "proxy-cb");
@@ -23,69 +30,102 @@ public class ConcertInfoCrawler
 		{
 			System.out.println(urlAndName);
 
-			try
+			for (String encode : encodes)
 			{
-				URL url = new URL(urlAndName.url);
-
-				InputStream in = url.openStream();
-				BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-				fileWriter.println(urlAndName.orchestra);
-				fileWriter.println("-----------------------------------------------");
-				String[] concertInfo = getConcertInfo(br);
-				for (String line : concertInfo)
+				try
 				{
-					if (line.length() > 100)
+					URL url = new URL(urlAndName.url);
+
+					InputStream in = url.openStream();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(in, encode));
+
+					fileWriter.println(urlAndName.orchestra);
+					fileWriter.println("-----------------------------------------------");
+					String[] concertInfo = getConcertInfo(reader);
+					if (concertInfo != null)
 					{
-						line = line.substring(0, 100);
+						// 情報あり
+
+						for (String line : concertInfo)
+						{
+							if (line.length() > 100)
+							{
+								line = line.substring(0, 100);
+							}
+
+							fileWriter.println(line);
+						}
+
+						break;
 					}
 
-					fileWriter.println(line);
+					in.close();
 				}
-
-				in.close();
-			}
-			catch (IOException exception)
-			{
-				fileWriter.println(exception.getMessage());
+				catch (IOException exception)
+				{
+					fileWriter.println(exception.getMessage());
+				}
 			}
 		}
 		fileWriter.close();
 	}
 
+	/**
+	 * HTMLからコンサート情報を取得
+	 * @param reader HTML読み込みオブジェクト
+	 * @return コンサート情報
+	 */
 	static public String [] getConcertInfo(BufferedReader reader)
 		throws IOException
 	{
 		int count = 0;
-		boolean range = false;
+		boolean find = false;
 		ArrayList<String> lines = new ArrayList<String>();
 		String line;
 		while ((line = reader.readLine()) != null)
 		{
 			line = line.replaceAll("<.+?>", "");
-			if (!range || count > 0)
+			line = line.trim();
+			if (line.length() <= 0)
 			{
+				// 空行
+
+				continue;
+			}
+
+			if (!find || count > 0)
+			{
+				// 演奏会情報の前or演奏会情報の範囲内
+
 				lines.add(line);
 				if (lines.size() > 30)
 				{
+					// 最大サイズを超えている
+
 					lines.remove(0);
 				}
 			}
 
-			if (range)
+			if (find)
 			{
+				// 演奏会情報の範囲内
+
 				count--;
 			}
 			else
 			{
+				// 演奏会情報の前
+
 				if (line.indexOf("演奏会") >= 0)
 				{
-					range = true;
+					// 演奏会の文字を含む
+
+					find = true;
 					count = 20;
 				}
 			}
 		}
 
-		return lines.toArray(new String [] {});
+		return find ? lines.toArray(new String [] {}) : null;
 	}
 }
