4bc2d49ac6295218b70ad74a535cd900b477ef0a
kumagai <iariro@gol.com>
2017-09-25 08:51:18 +0900

    日付の認識パターンを追加

--- src/kumagai/concert/crawler/DateUtility.java
+++ src/kumagai/concert/crawler/DateUtility.java
@@ -9,13 +9,15 @@ import java.util.regex.Pattern;
  */
 public class DateUtility
 {
-	static public final Pattern pattermSeirekiHalf =
-		Pattern.compile(".*([0-9]{4})[/／年] *([0-9]{1,2})[/／月] *([0-9]{1,2})[／日]*.*");
-	static public final Pattern pattermSeirekiFull =
-			Pattern.compile(".*([０-９]{4})[/／年] *([０-９]{1,2})[/／月] *([０-９]{1,2})[／日]*.*");
-	static public final Pattern pattermWarekiHalf =
+	static public final Pattern patternSeirekiHalf =
+		Pattern.compile(".*([0-9]{4}) *[/／年\\.][ ）]*([0-9]{1,2}) *[/／月\\.] *([0-9]{1,2}) *[／日]*.*");
+	static public final Pattern patternSeirekiFull =
+		Pattern.compile(".*([0-9０-９]{4}) *[/／年\\.][ ）]*([0-9０-９]{1,2}) *[/／月\\.] *([0-9０-９]{1,2}) *[／日]*.*");
+	static public final Pattern patternSeireki2keta =
+		Pattern.compile(".*'([0-9]{2})/([0-9]{1,2})/([0-9]{1,2}).*");
+	static public final Pattern patternWarekiHalf =
 		Pattern.compile(".*平成([0-9]*)[/／年] *([0-9]{1,2})[/／月] *([0-9]{1,2})[／日]*.*");
-	static public final Pattern pattermWarekiFull =
+	static public final Pattern patternWarekiFull =
 		Pattern.compile(".*平成([０-９]*)[/／年] *([０-９]{1,2})[/／月] *([０-９]{1,2})[／日]*.*");
 
 	/**
@@ -27,7 +29,7 @@ public class DateUtility
 	{
 		date = changeNumFullToHalf(date);
 
-		Matcher matcher = pattermSeirekiHalf.matcher(date);
+		Matcher matcher = patternSeirekiHalf.matcher(date);
 		if (matcher.matches())
 		{
 			int year = Integer.valueOf(matcher.group(1));
@@ -35,13 +37,21 @@ public class DateUtility
 			int day = Integer.valueOf(matcher.group(3));
 			return changeNumFullToHalf(String.format("%04d/%02d/%02d", year, month, day));
 		}
-		matcher = pattermWarekiHalf.matcher(date);
+		matcher = patternWarekiHalf.matcher(date);
 		if (matcher.matches())
 		{
 			int year = Integer.valueOf(matcher.group(1));
 			int month = Integer.valueOf(matcher.group(2));
 			int day = Integer.valueOf(matcher.group(3));
 			return changeNumFullToHalf(String.format("%04d/%02d/%02d", year + 1988, month, day));
+		}
+		matcher = patternSeireki2keta.matcher(date);
+		if (matcher.matches())
+		{
+			int year = Integer.valueOf(matcher.group(1));
+			int month = Integer.valueOf(matcher.group(2));
+			int day = Integer.valueOf(matcher.group(3));
+			return changeNumFullToHalf(String.format("%04d/%02d/%02d", year + 2000, month, day));
 		}
 		return null;
 	}
--- src/kumagai/concert/test/DateUtilityTest.java
+++ src/kumagai/concert/test/DateUtilityTest.java
@@ -8,12 +8,17 @@ public class DateUtilityTest
 {
 	public void testTrimDate()
 	{
+		assertEquals("2015/08/15", DateUtility.trimDate("'15/08/15"));
+		assertEquals("2017/07/28", DateUtility.trimDate("2017.7.28"));
 		assertEquals("2017/01/02", DateUtility.trimDate("2017/01/02"));
 		assertEquals("2017/03/04", DateUtility.trimDate("2017年3月4日"));
+		assertEquals("2013/05/26", DateUtility.trimDate("2013 年 5 月 26 日"));
+		assertEquals("2017/07/23", DateUtility.trimDate("2017年７月23日"));
 		assertEquals("2017/05/05", DateUtility.trimDate("2017年 5月 5日"));
 		assertEquals("2017/05/06", DateUtility.trimDate("２０１７年５月６日"));
 		assertEquals("2017/07/08", DateUtility.trimDate("２０１7年７月８日"));
 		assertEquals("2017/09/10", DateUtility.trimDate("平成29年9月10日"));
 		assertEquals("2017/11/12", DateUtility.trimDate("平成２９年１１月１２日"));
+		assertEquals("2016/01/10", DateUtility.trimDate("平成２８年（2016年）1月10日"));
 	}
 }
