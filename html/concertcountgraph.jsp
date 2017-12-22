01b806774a6c9ef7a7bcefadd9b2121b3908c025
kumagai <kumagai@toshiba-tops.co.jp>
2017-12-22 12:48:53 +0900

    登録日ごとの登録件数リスト表示を折れ線グラフ化 実装開始

---
+++ html/concertcountgraph.jsp
@@ -0,0 +1,33 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+
+<%@ taglib uri="/struts-tags" prefix="s" %>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv=Content-Style-Type content=text/css>
+		<link rel="stylesheet" type="text/css" href="hatena.css">
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script type="text/javascript" src="http://code.highcharts.com/highcharts.js"></script>
+		<title>登録件数</title>
+	</head>
+
+	<body>
+		<div id="chart" style="width:1300px; height:700px"></div>
+		<script type="text/javascript">
+		function draw()
+		{
+			Highcharts.setOptions({ global: { useUTC: false } });
+			options =
+			{
+				chart: {renderTo: 'chart', zoomType:'xy', plotBackgroundColor: 'lightgray'},
+				title: {text: '登録件数'},
+				xAxis: {title: null, type: 'datetime'},
+				series: [ <s:property value='concertCountPoints' /> ]
+			};
+			chart = new Highcharts.Chart(options);
+		};
+		document.body.onload = draw();
+		</script>
+	</body>
+</html>
--- src/kumagai/concert/struts2/ConcertCountAction.java
+++ src/kumagai/concert/struts2/ConcertCountAction.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
@@ -11,6 +12,7 @@ import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
@@ -22,32 +24,83 @@ import kumagai.concert.ConcertCollection;
  * @author kumagai
  */
 @Namespace("/concert")
-@Result(name="success", location="/concert/concertcount.jsp")
+@Results
+({
+	@Result(name="success", location="/concert/concertcountgraph.jsp"),
+	@Result(name="error", location="/concert/error.jsp")
+})
 public class ConcertCountAction
 {
 	public LinkedHashMap<DateTime, Integer> concertCount;
+	public String message;
+
+	/**
+	 * hiChartsグラフ用座標文字列を取得
+	 * @return hiChartsグラフ用座標文字列
+	 */
+	public String getConcertCountPoints()
+	{
+		StringBuffer highChartsString = new StringBuffer();
+		highChartsString.append("{name: '件数',data: [");
+		int count = 0;
+
+		for (Map.Entry<DateTime, Integer> entry : concertCount.entrySet())
+		{
+			if (count > 0)
+			{
+				// ２個目以降
+
+				highChartsString.append(",");
+			}
+			highChartsString.append(String.format("[Date.parse('%s'), %d]", entry.getKey(), entry.getValue()));
+			count++;
+		}
+		highChartsString.append("]}");
+
+		return highChartsString.toString();
+	}
 
 	/**
 	 * 更新日ごとの登録件数表示アクション。
 	 * @return 処理結果
-	 * @throws Exception
 	 */
 	@Action("concertcount")
 	public String execute()
 		throws Exception
 	{
-		ServletContext context = ServletActionContext.getServletContext();
+		try
+		{
+			ServletContext context = ServletActionContext.getServletContext();
 
-		DriverManager.registerDriver(new SQLServerDriver());
+			String dbUrl = context.getInitParameter("ConcertSqlserverUrl");
+			if (dbUrl != null)
+			{
+				// パラメータあり
 
-		Connection connection =
-			DriverManager.getConnection
-				(context.getInitParameter("ConcertSqlserverUrl"));
-		Statement statement = connection.createStatement();
-		concertCount = ConcertCollection.getConcertCountPerUpdateDate(connection, statement);
-		statement.close();
-		connection.close();
+				DriverManager.registerDriver(new SQLServerDriver());
 
-		return "success";
+				Connection connection = DriverManager.getConnection(dbUrl);
+				Statement statement = connection.createStatement();
+				concertCount = ConcertCollection.getConcertCountPerUpdateDate(connection, statement);
+				statement.close();
+				connection.close();
+
+				return "success";
+			}
+			else
+			{
+				// パラメータなし
+
+				message = "ConcertSqlserverUrl定義なし";
+			}
+		}
+		catch (Exception exception)
+		{
+			// 何もしない
+
+			message = exception.toString();
+		}
+
+		return "error";
 	}
 }
