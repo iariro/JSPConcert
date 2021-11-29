391ad78cc52de25afd62786549f2518c0c2e04c4
kumagai <iariro@gol.com>
2021-11-29 22:03:31 +0900

    コンサートが終わったオーケストラ取得をサーブレット化 #6

---
+++ src/kumagai/concert/servlet/PastOrchestraListServlet.java
@@ -0,0 +1,60 @@
+package kumagai.concert.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
+public class PastOrchestraListServlet
+	extends HttpServlet
+{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException
+	{
+		try
+		{
+			DriverManager.registerDriver(new SQLServerDriver());
+			Connection connection = DriverManager.getConnection(getInitParameter("ConcertSqlserverUrl"));
+			String sql = "select Player.name as title, Player.siteurl as url, Player.siteencoding, Player.active, max(Concert.date) as lastdate from Concert " +
+				"join Shutsuen on Shutsuen.concertId=Concert.id " +
+				"join Player on Player.id=Shutsuen.playerId " +
+				"where Shutsuen.partId=1 and len(Player.siteurl)>0 and Player.active=1 " +
+				"group by player.id, Player.name, Player.siteurl, Player.siteencoding, Player.active " +
+				"having max(Concert.date) < getdate() order by max(Concert.date)";
+
+			PreparedStatement statement = connection.prepareStatement(sql);
+			ResultSet result = statement.executeQuery();
+
+			request.setCharacterEncoding("utf-8");
+			response.setContentType("text/plain; charset=UTF-8");
+
+			PrintWriter writer = response.getWriter();
+			while (result.next())
+			{
+				String title = result.getString("title");
+				String url = result.getString("url");
+				String siteencoding = result.getString("siteencoding");
+				int active = result.getInt("active");
+				String lastdate = result.getString("lastdate");
+				writer.printf("%s,%s,%s,%d,%s\n", title, url, siteencoding, active, lastdate);
+			}
+		}
+		catch (SQLException e)
+		{
+			PrintWriter writer = response.getWriter();
+			writer.println(e.toString());
+			e.printStackTrace();
+		}
+	}
+}
